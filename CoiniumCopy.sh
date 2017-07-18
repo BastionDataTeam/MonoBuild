21df541da4baa2ca53459216bf64930c0fef2993
PadsterH2012 <paddy@bastiondata.com>
2017-07-18 21:59:08 +0100

    Update CoiniumCopy.sh

--- CoiniumCopy.sh
+++ CoiniumCopy.sh
@@ -1,3 +1,3 @@
 # /bin/bash
 
-wget -r -nH -nd -np -R http://10.0.0.211/build/CoiniumServ/bin/Release/ ./CoiniumServ
+wget -r -nH -nd -np -R http://10.0.0.211/build/CoiniumServ/bin/Release/ /CoiniumServ/
